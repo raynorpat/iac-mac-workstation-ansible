601973b956ddae49e495e52d2a067cca19942b3d
Pat Raynor <raynorpat@raynorpat.com>
2024-04-24 18:08:49 -0400

    Simplify main.yml a bit

--- run-setup.sh
+++ run-setup.sh
@@ -1,15 +1,21 @@
 # step 1
-# install homeberw - $ /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+# install homebrew
+#/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 # step 2
+# update homebrew
+brew update
+
+# step 3
 # install ansible - brew install ansible
 brew install ansible
 
+# step 4
 # check for any homebrew issues
 brew doctor
 
-# step 3 - run the ansible playbook to install the reqs
+# step 5 - run the ansible playbook to install the reqs
 ansible-galaxy install -r requirements.yml
 
-# step 4 - run the ansible playbook, enter the local account credentials when prompted
-ansible-playbook main.yml --ask-become-pas
+# step 6 - run the ansible playbook, enter the local account credentials when prompted
+ansible-playbook main.yml --ask-become-pass
